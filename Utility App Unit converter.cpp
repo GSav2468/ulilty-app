cbabcecf63961619e119c30367763384833a02a8
GSav2468 <Gurdave.Singh24@bathspa.ac.uk>
2026-02-22 11:16:37 +0000

    Initial setup and variable declaration

---
+++ Utility App Unit converter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// --- CHAPTER 1: VARIABLES ---
+// These variables store user input and control program flow
+int userChoice = 0;
+double userInput = 0.0;
+bool isRunning = true;
+
+int main() {
+    return 0;
+}
